ba63218e9fc78e53e58fb1f374c1d63a2a9b6b9f
Aparna19Soni <aparna19soni@gmail.com>
2025-06-10 16:45:56 -0700

    two pointers

---
+++ src/main/java/com/aparna/DSPractice/TwoPointers/ReverseString.java
@@ -0,0 +1,19 @@
+package com.aparna.DSPractice.TwoPointers;
+
+public class ReverseString {
+    public static void main(String[] args) {
+        char[] arr ={'h','e','l','l','o'};
+        int i =0;
+        int j =arr.length-1;
+        while(i<j)
+        {
+            char temp =arr[i];
+            arr[i]=arr[j];
+            arr[j]=temp;
+            i++;
+           j--;
+        }
+        System.out.println(arr);
+    }
+
+}
---
+++ src/main/java/com/aparna/DSPractice/TwoPointers/SquaresOfASortedArray.java
@@ -0,0 +1,26 @@
+package com.aparna.DSPractice.TwoPointers;
+
+import java.util.Arrays;
+
+public class SquaresOfASortedArray {
+    public static void main(String[] args) {
+        int[] arr = {-4, -1, 0, 3, 10};
+        int[] sortedSquare = new int[arr.length];
+        int i = 0;
+        int j = arr.length - 1;
+        int k = arr.length - 1;
+
+        while (i < j) {
+            if (arr[j] * arr[j] > arr[i] * arr[i]) {
+                sortedSquare[k] = arr[j] * arr[j];
+                k--;
+                j--;
+            } else if (arr[j] * arr[j] < arr[i] * arr[i]) {
+                sortedSquare[k] = arr[i] * arr[i];
+                k--;
+                i++;
+            }
+        }
+        System.out.println(Arrays.toString(sortedSquare));
+    }
+}
